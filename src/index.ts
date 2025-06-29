9ca0938f01f79c158383989b22b948f41fc09498
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-29 14:41:25 +0300

    Add static file hosting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import express from "express";
 
-const app = express().listen(8080);
+const app = express();
+const PORT = 8080;
+
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
