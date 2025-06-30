79eb760bbd6407bbd82272879a78bf873bca5b4b
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-30 13:35:35 +0300

    Add chirp validation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { handleMetrics, handleReset } from "./api/metrics.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
+import { handlerValidateChirp } from "./api/validation.js";
 
 const app = express();
 const PORT = 8080;
@@ -11,8 +12,9 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use(middlewareLogResponses);
 
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerValidateChirp);
 app.get("/admin/metrics", handleMetrics);
-app.get("/admin/reset", handleReset);
+app.post("/admin/reset", handleReset);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
