7c6101ae4a8b211f1edebb0bb712e609a7d9219a
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-30 18:48:20 +0300

    Add users table

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile();
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
     fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string) {
+    const val = process.env[key];
+    if (!val) {
+        throw new Error(`Environment variable ${key} must be set`);
+    }
+    return val;
+}
